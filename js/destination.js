b3a73b59f3aa425d4dad54927150679df4e1e019
Guilherme Zalla <zallaguilherme@gmail.com>
2022-02-14 14:40:34 -0300

    updated javascript responsive pictures

--- js/destination.js
+++ js/destination.js
@@ -13,7 +13,13 @@ function changeDestination(element) {
         case 'moon':
             destination.planets.setAttribute("src", "assets/destination/image-moon.png");
             destination.planetTitle.innerText = 'MOON';
-            destination.planetDescription.innerHTML = "See our planet as you've never seen it before. A perfect <br> relaxing trip away to help regain perspective and come <br> back refreshed. While you're there, take in some history <br> by visiting the Luna 2 and Apollo 11 landing sites."
+            if (window.matchMedia("(min-width: 601px) and (max-width: 990px").matches) {
+                destination.planetDescription.innerHTML = "See our planet as you've never seen it before. A perfect  relaxing trip away to help<br> regain perspective and come  back refreshed. While you're there, take in some <br> history by visiting the Luna 2 and Apollo 11 landing sites."
+            } else if (window.matchMedia("(max-width: 600px").matches) {
+                destination.planetDescription.innerHTML = "See our planet as you've never seen it before. A <br> perfect relaxing trip away to help regain <br> perspective and come back refreshed. While <br> you're there, take in some history by visiting the <br> Luna 2 and Apollo 11 landing sites."
+            } else {
+                destination.planetDescription.innerHTML = "See our planet as you've never seen it before. A perfect <br> relaxing trip away to help regain perspective and come <br> back refreshed. While you're there, take in some history <br> by visiting the Luna 2 and Apollo 11 landing sites."
+            }
             destination.planetDistance.innerHTML = "384,400 KM";
             destination.planetTravelTime.innerHTML = "3 DAYS";
             element.classList.add("is-active-destination");
@@ -26,7 +32,13 @@ function changeDestination(element) {
         case 'mars':
             destination.planets.setAttribute("src", "assets/destination/image-mars.png");
             destination.planetTitle.innerText = 'MARS';
-            destination.planetDescription.innerHTML = "Don't forget to pack your hiking boots. You'll need them to <br> tackle Olympus Mons, the tallest planetary mountain in <br> our solar system. It's two and a half times the size of <br> Everest!";
+            if (window.matchMedia("(min-width: 601px) and (max-width: 990px").matches) {
+                destination.planetDescription.innerHTML = "Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the <br> tallest planetary mountain in our solar system. It's two and a half times the size of <br> Everest!";
+            } else if (window.matchMedia("(max-width: 600px").matches) {
+                destination.planetDescription.innerHTML = "Don't forget to pack your hiking boots. You'll need <br> them to tackle Olympus Mons, the tallest planetary <br> mountain in our solar system. It's two and a half<br> times the size of Everest!";
+            } else {
+                destination.planetDescription.innerHTML = "Don't forget to pack your hiking boots. You'll need them to <br> tackle Olympus Mons, the tallest planetary mountain in <br> our solar system. It's two and a half times the size of <br> Everest!";
+            }
             destination.planetDistance.innerHTML = "225 MIL. KM";
             destination.planetTravelTime.innerHTML = "9 MONTHS";
             element.classList.add("is-active-destination");
@@ -39,7 +51,13 @@ function changeDestination(element) {
         case 'europa':
             destination.planets.setAttribute("src", "assets/destination/image-europa.png");
             destination.planetTitle.innerText = 'EUROPA';
-            destination.planetDescription.innerHTML = "The smallest of the four Galilean moons orbiting Jupiter,<br>Europa is a winter lover's dream. With an icy surface, it's<br>perfect for a bit of ice skating, curling, hockey or simple<br>relaxation in your snug wintery cabin.";
+            if (window.matchMedia("(min-width: 601px) and (max-width: 990px").matches) {
+                destination.planetDescription.innerHTML = "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's<br> dream. With an icy surface, it's perfect for a bit of ice skating, curling, hockey or<br>simple relaxation in your snug wintery cabin.";
+            } else if (window.matchMedia("(max-width: 600px").matches) {
+                destination.planetDescription.innerHTML = "The smallest of the four Galilean moons orbiting <br>Jupiter, Europa is a winter lover's dream. With an <br> icy surface, it's perfect for a bit of ice skating, <br> curling, hockey or simple relaxation in your snug <br> wintery cabin.";
+            } else {
+                destination.planetDescription.innerHTML = "The smallest of the four Galilean moons orbiting Jupiter,<br>Europa is a winter lover's dream. With an icy surface, it's<br>perfect for a bit of ice skating, curling, hockey or simple<br>relaxation in your snug wintery cabin.";
+            }
             destination.planetDistance.innerHTML = "628 MIL. KM";
             destination.planetTravelTime.innerHTML = "3 YEARS";
             for (let i = 0; i < destination.destinationLink.length; ++i) {
@@ -51,7 +69,13 @@ function changeDestination(element) {
         case 'titan':
             destination.planets.setAttribute("src", "assets/destination/image-titan.png");
             destination.planetTitle.innerText = 'TITAN';
-            destination.planetDescription.innerHTML = "The only moon known to have a dense atmosphere other <br> than Earth, Titan is a home away from home (just a few <br>hundred degrees colder!). As a bonus, you get striking<br>views of the Rings of Saturn.";
+            if (window.matchMedia("(min-width: 601px) and (max-width: 990px").matches) {
+                destination.planetDescription.innerHTML = "The only moon known to have a dense atmosphere other than Earth, Titan is a <br> home away from home (just a few hundred degrees colder!). As a bonus, you get <br> striking views of the Rings of Saturn.";
+            } else if (window.matchMedia("(max-width: 600px").matches) {
+                destination.planetDescription.innerHTML = "The only moon known to have a dense atmosphere <br> other than Earth, Titan is a home away from home <br> (just a few hundred degrees colder!). As a bonus, <br> you get striking views of the Rings of Saturn.";
+            } else {
+                destination.planetDescription.innerHTML = "The only moon known to have a dense atmosphere other <br> than Earth, Titan is a home away from home (just a few <br>hundred degrees colder!). As a bonus, you get striking<br>views of the Rings of Saturn.";
+            }
             destination.planetDistance.innerHTML = "1.6 BIL. KM";
             destination.planetTravelTime.innerHTML = "7 YEARS";
             for (let i = 0; i < destination.destinationLink.length; ++i) {
--- js/technology.js
+++ js/technology.js
@@ -2,7 +2,8 @@ const technologies = {
     button: document.querySelectorAll("button"),
     title: document.querySelector(".technology__title--text"),
     text: document.querySelector(".technology__text"),
-    image: document.querySelector(".technology__img")
+    image: document.querySelector(".technology__img"),
+    landscape: document.querySelector(".landscape")
 }
 
 technologies.button.forEach(button => {
@@ -11,7 +12,11 @@ technologies.button.forEach(button => {
             case 'launch':
                 technologies.title.innerHTML = 'LAUNCH VEHICLE';
                 technologies.text.innerHTML = "A launch vehicle or carrier rocket is a rocket-propelled<br>vehicle used to carry a payload from Earth's surface to <br>space, usually to Earth orbit or beyond. Our WEB-X<br>carrier rocket is the most powerful in operation. Standing<br>150 metres tall, it's quite an awe-inspiring sight on the<br>launch pad!"
-                technologies.image.setAttribute("src", "/assets/technology/image-launch-vehicle-portrait.jpg");
+                if (window.matchMedia("(max-width: 990px)").matches) {
+                    technologies.landscape.setAttribute("srcset", "/assets/technology/image-launch-vehicle-landscape.jpg");
+                } else {
+                    technologies.image.setAttribute("src", "/assets/technology/image-launch-vehicle-portrait.jpg");
+                }
                 for (let i = 0; technologies.button.length; ++i) {
                     if (technologies.button[i].getAttribute("data-tech") != 'launch') {
                         technologies.button[i].classList.remove("is-active-technology");
@@ -23,7 +28,11 @@ technologies.button.forEach(button => {
             case 'spaceport':
                 technologies.title.innerHTML = 'SPACEPORT';
                 technologies.text.innerHTML = "A spacepor or cosmodrome is a site for launching (or<br>receiving) spacecraft, by analogy to the seaport for ships or<br>airport for aircraft. Based in the famous Cape <br>Canaveral, our spaceport is ideally situated to take<br>advantage of the Earth's rotation for launch."
-                technologies.image.setAttribute("src", "/assets/technology/image-spaceport-portrait.jpg");
+                if (window.matchMedia("(max-width: 990px)").matches) {
+                    technologies.landscape.setAttribute("srcset", "/assets/technology/image-spaceport-landscape.jpg");
+                } else {
+                    technologies.image.setAttribute("src", "/assets/technology/image-spaceport-portrait.jpg");
+                }
                 for (let i = 0; technologies.button.length; ++i) {
                     if (technologies.button[i].getAttribute("data-tech") != 'spaceport') {
                         technologies.button[i].classList.remove("is-active-technology");
@@ -35,7 +44,11 @@ technologies.button.forEach(button => {
             case 'capsule':
                 technologies.title.innerHTML = 'SPACE CAPSULE';
                 technologies.text.innerHTML = "A space capsule is an often-crewed spacecraft that uses<br>a blunt-body reentry capsule to reenter the Earth's<br>atmosphere without wings. Our capsule is where you'll<br>spend your time during the flight.It includes a space<br> gym, cinema, and plenty of other activities to keep you<br>entertained.";
-                technologies.image.setAttribute("src", "/assets/technology/image-space-capsule-portrait.jpg");
+                if (window.matchMedia("(max-width: 990px)").matches) {
+                    technologies.landscape.setAttribute("srcset", "/assets/technology/image-space-capsule-landscape.jpg");
+                } else {
+                    technologies.image.setAttribute("src", "/assets/technology/image-space-capsule-portrait.jpg");
+                }
                 for (let i = 0; technologies.button.length; ++i) {
                     if (technologies.button[i].getAttribute("data-tech") != 'capsule') {
                         technologies.button[i].classList.remove("is-active-technology");
